5d476d396c882fe5b90ec2bf3e2bf02fcbbf959e
Lina Charane <charaneluna@gmail.com>
2026-08-19 17:45:54 +0200

    added jupyter notebook and micrograd still ongoing

--- LEARNING/Andrej Karpathy/bacpropagation.py
+++ LEARNING/Andrej Karpathy/bacpropagation.py
@@ -1,4 +1,4 @@
-
+import math
 
 #  This will be an example of manual backpropagation through a perceptron of two layers 
 
@@ -21,26 +21,57 @@ class Value:
         out = Value(self.data * other.data, (self,other), '*')
         return out
 
-    
-def back():
-    h = 0.001
-    a =Value(2.0, label = 'a')
-    b= Value (-3.0, label = 'b')
-    c = Value (10.0, label = 'c')
-    e = a*c, e.label = 'e'
-    d = e+c, d.label = 'd'
-    f = Value (-2.0, label = 'f')
-    L = d+f, L.label = 'L'
-    l1 = L.data
+    def tanh(self): # since this is inside of the class we cant do tanh(h) but instead we do n.tanh
+        x = self.data
+        t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+        return Value(t,(self,), 'tanh')
 
-    a =Value(2.0, label = 'a')
-    b= Value (-3.0, label = 'b')
-    c = Value (10.0, label = 'c')
-    e = a*c, e.label = 'e'
-    d = e+c, d.label = 'd'
-    f = Value (-2.0, label = 'f')
-    L = d+f, L.label = 'L'
-    l2 = L.data 
+#brief example of a simplified manual backpropagation 
+
+# def back():
+
+#     h = 0.001
+#     a =Value(2.0, label = 'a')
+#     b= Value (-3.0, label = 'b')
+#     c = Value (10.0, label = 'c')
+#     e = a*c, e.label = 'e'
+#     d = e+c, d.label = 'd'
+#     f = Value (-2.0, label = 'f')
+#     L = d+f, L.label = 'L'
+#     l1 = L.data
+
+#     a =Value(2.0, label = 'a')
+#     b= Value (-3.0, label = 'b')
+#     c = Value (10.0, label = 'c')
+#     e = a*c, e.label = 'e'
+#     d = e+c, d.label = 'd'
+#     f = Value (-2.0, label = 'f')
+#     L = d+f, L.label = 'L'
+#     l2 = L.data 
+
+# end of manual backpropagation example 
+
+
+def back2():
+    #inputs
+    x1= Value(2.0,label = 'x1')
+    x2= Value(0.0,label = 'x2')
+    #weights
+    w1=Value(-3.0,label='w1')
+    w2=Value(1.0,label='w2')
+    #bias
+    b= Value(6.7,label='b')
+    # x1*w1 and x2*w2
+
+    x1w1 = x1*w1 ; label= 'x1w1'
+    x2w2= x2*w2; label = 'x2w2'
+
+    x1w1x2w2= x1w1+x2w2 ; label = 'x1w1x2w2'
+
+    n= x1w1x2w2 + b
+    o = n.tanh() # this introduces a non linear function to the system so that it can have more complex thinking and not just a linear thinking
+    
+
 
 
 
